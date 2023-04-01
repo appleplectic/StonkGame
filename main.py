bffd16628adf4970e53952996dcf563dcbb6205c
Appleplectic <llmar1000000@gmail.com>
2023-04-01 10:41:43 -0400

    bad

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from pynput import keyboard
 
-if __name__ == "__main__":
-    class Stock():
-        def __init__(self, name, symbol, price, industry, per, cap, volume):
-            self.name = name
-            self.price = price
-            self.industry = industry
-            self.symbol = symbol
-            self.per = per
-            self.cap = cap
-            self.volume = volume
-            return
-        def __str__(self):
-            return str(self.name, "(" + self.symbol + ") $" + self.price)
-    
 
+class Stock():
+    def __init__(self, name, symbol, price, industry, per, cap, volume):
+        self.name = name
+        self.price = price
+        self.industry = industry
+        self.symbol = symbol
+        self.per = per
+        self.cap = cap
+        self.volume = volume
+        return
+
+    def __str__(self):
+        return str(self.name, "(" + self.symbol + ") $" + self.price)
+
+if __name__ == "__main__":
     print("Hello World!")
