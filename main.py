57739c9c87a01a46bd841774045824825ec4eb01
Holden Lim <holdenclim@gmail.com>
2023-03-31 22:16:24 -0400

    Messing around

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from pynput import keyboard
 
 if __name__ == "__main__":
     class Stock():
-        def __init__(self, name, symbol, per, cap):
+        def __init__(self, name, symbol, price, industry, per, cap, volume):
             self.name = name
+            self.price = price
+            self.industry = industry
             self.symbol = symbol
             self.per = per
             self.cap = cap
+            self.volume = volume
+            return
+        def __str__(self):
+            return str(self.name, "(" + self.symbol + ") $" + self.price)
+    
 
-
-    print("Butthole Milkshake")
+    print("Hello World!")
