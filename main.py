24cb08824c57d4ca4172310e62dea85f7bc51f1e
Holden Lim <holdenclim@gmail.com>
2023-03-31 22:11:40 -0400

    messing around

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from pynput import keyboard
 
 if __name__ == "__main__":
+    class Stock():
+        def __init__(self, name, symbol, per, cap):
+            self.name = name
+            self.symbol = symbol
+            self.per = per
+            self.cap = cap
+
+
     print("Butthole Milkshake")
